0967d5cb6976539d740ced146eb72f8eb4ed8537
yoomster <nperenboom@hotmail.com>
2025-03-24 17:10:23 +0100

    login page added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <v-app> </v-app>
+  <v-app>
+    <v-card>
+      <v-card-title>
+        <h1 class="display-1">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field label="Email" />
+          <v-text-field type="password" label="Wachtwoord" />
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn>Registreer</v-btn>
+        <v-btn>Login</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-app>
 </template>
 
 <script lang="ts" setup>
