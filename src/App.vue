9776678dd6f4a2ae3b84c99136e4934388b77932
yoomster <nperenboom@hotmail.com>
2025-03-26 21:11:32 +0100

    Added show password option for text or hide

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <v-app>
-    <v-card>
+    <v-card width="500" class="mx-auto mt-5">
       <v-card-title>
         <h1 class="display-1">Login</h1>
       </v-card-title>
       <v-card-text>
         <v-form>
-          <v-text-field label="Email" />
-          <v-text-field type="password" label="Wachtwoord" />
+          <v-text-field label="Email" prepend-icon="mdi-account-circle" />
+          <v-text-field
+            :type="showPassword ? 'text' : 'password'"
+            label="Wachtwoord"
+            prepend-icon="mdi-lock"
+            append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+          />
         </v-form>
       </v-card-text>
+      <v-divider> </v-divider>
       <v-card-actions>
-        <v-btn>Registreer</v-btn>
-        <v-btn>Login</v-btn>
+        <v-btn color="success">Registreer</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">Login</v-btn>
       </v-card-actions>
     </v-card>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-//
+const showPassword = false;
 </script>
